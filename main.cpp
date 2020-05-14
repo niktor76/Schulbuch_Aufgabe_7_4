d8a4f3ae16594c0124194bbeb5f415fd8e7207d6
viktor-nbubar <viktor.steck@gmail.com>
2020-05-14 20:23:11 +0200

    blabla

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 int main() {
-    const int columns = 2;
-    const int rows = 2;
-    int matrize1[rows][columns] = {{1, 2},
-                                   {3, 4}};
-    int matrize2[rows][columns] = {{5,6},
-                                   {7,8}};
+    const int columns = 3;
+    const int rows = 3;
+    int matrize1[rows][columns] = {{1, 2, 3},
+                                   {4, 5, 6},
+                                   {7, 8, 9}};
+    int matrize2[rows][columns] = {{10, 11, 12},
+                                   {13, 14, 15},
+                                   {16, 17, 18}};
     int product[rows][columns];
+
     for (int r = 0; r < rows; ++r) {
         for (int c = 0; c < columns; ++c) {
             int sum = 0;
@@ -19,11 +22,13 @@ int main() {
             product[r][c] = sum;
         }
     }
+
     for (int r = 0; r < rows; ++r) {
         for (int c = 0; c < columns; ++c) {
             cout << product[r][c] << " ";
         }
         cout << endl;
     }
+
     return 0;
 }
